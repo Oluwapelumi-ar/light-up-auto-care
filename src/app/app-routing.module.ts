ae66d25033df34939bf1036bb25477b5b56b05f9
Adebiyi Victor <victorbiyi2010@gmail.com>
2021-09-03 17:11:25 +0100

    my first commmit working on quote page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
+import { Quote } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { QuoteComponent } from './quote/quote.component';
 import { ServicesComponent } from './services/services.component';
+import { StaffComponent } from './staff/staff.component';
+
 
 
 const routes: Routes = [
   { path:'home', component:HomeComponent},
   { path:'services', component:ServicesComponent},
+  { path:'quote', component:QuoteComponent},
+  { path: 'invoice', component:InvoiceComponent},
+  { path: 'staff', component:StaffComponent},
   { path:'' ,loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
 ];
 
@@ -15,3 +23,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 import { ServicesComponent } from './services/services.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { SideMenuComponent } from './layout/side-menu/side-menu.component';
+import { QuoteComponent } from './quote/quote.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { StaffComponent } from './staff/staff.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
@@ -19,12 +22,17 @@ import { SideMenuComponent } from './layout/side-menu/side-menu.component';
     ServicesComponent,
     HeaderComponent,
     SideMenuComponent,
+    QuoteComponent,
+    InvoiceComponent,
+    StaffComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
