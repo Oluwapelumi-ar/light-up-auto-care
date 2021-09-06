78b67314a8c6d6e594167297abd4a96a95dbdff9
Chidera Alakwe <calakwe@atbtechsoft.com>
2021-09-06 13:02:46 +0100

    modification on client page

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -11,6 +11,7 @@ import { ClientModalComponent } from './client-modal/client-modal.component';
 })
 export class ClientComponent implements OnInit {
   clientData: any = [];
+  formValue: any;
 
   constructor(private modalService: NgbModal, private api: ApiService) {}
   ngOnInit(): void {
@@ -25,9 +26,21 @@ export class ClientComponent implements OnInit {
   }
 
   getAllClient() {
-    this.api.getClient().subscribe((res) => {
-      this.clientData = [res.payload];
-      // console.log(this.clientData, 're');
+    this.api.getAllClients().subscribe((res) => {
+      this.clientData = res.payload;
     });
+  }
+
+  deleteClient(row: any) {
+    this.api.deleteClient(row.id).subscribe((res) => {
+      alert('Client deleted successfully ');
+      this.getAllClient();
+    });
+  }
+
+  onEdit(row: any) {
+    this.formValue.controls['name'].setValue(row.name);
+    this.formValue.controls['email'].setValue(row.email);
+    this.formValue.controls['phone'].setValue(row.phone);
   }
 }
